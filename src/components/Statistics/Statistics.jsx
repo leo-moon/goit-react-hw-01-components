eecff3a76747d6bf87c9006b0dde166c2b033d15
leo-moon <49075905+leo-moon@users.noreply.github.com>
2023-01-18 16:37:22 +0200

    helper

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,5 +1,5 @@
 import './statistics.css';
-import bgcs from './colors.json'
+// import bgcs from './colors.json'
 
 const Statistics = ({ stats })=> {
   const elementsLi = stats.map(stat => 
@@ -8,11 +8,11 @@ const Statistics = ({ stats })=> {
       <span className="percentage">{stat.percentage}%</span>
     </li>
     )
-  const bgcsLength = bgcs.length
-  for (let i=0 ; i < elementsLi.length; i+= 1) {
-    i = i % bgcsLength
-    console.log(i)
-  }
+  // const bgcsLength = bgcs.length
+  // for (let i=0 ; i < elementsLi.length; i+= 1) {
+  //   i = i % bgcsLength
+  //   console.log(i)
+  // }
 
   return (
     <section className="statistics">
