4f2f24a98f6e0423542f936076c95c78867ad6a8
Vladimir Larin <lvv0820@gmail.com>
2023-01-19 21:31:38 +0200

    vs-code change settings

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,55 +7,22 @@ import FriendList from './FriendList/FriendList';
 import TransactionHistory from './TransactionHistory/TransactionHistory';
 import transactions from './TransactionHistory/transactions.json';
 
-
 export const App = () => {
   return (
-  <>
-  <h3>Task #1</h3>
-
-  <Profile
-    username={user.username}
-    tag={user.tag}
-    location={user.location}
-    avatar={user.avatar}
-    stats={user.stats}
-  />
-
-  <h3>Task #2</h3>
-  <Statistics title="Upload stats" stats={data} />
-  <h3>Task #2 without title</h3>
-  <Statistics stats={data} />
-
-  <h3>Task #3</h3>
-  <FriendList friends={friends} />; 
-
-  <h3>Task #4</h3>
-  <TransactionHistory items={transactions} />;
-  </>
-  )
-}
-
-
-
-
-
-
-
-// export const App = () => {
-//   return (
-//     <div
-//       style={{
-//         height: '100vh',
-//         display: 'flex',
-//         justifyContent: 'center',
-//         alignItems: 'center',
-//         fontSize: 40,
-//         color: '#010101'
-//       }}
-//     >
-    
-//       React homework template
-//       <Profile />
-//     </div>
-//   );
-// };
+    <>
+      <h3>Task #1</h3>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <h3>Task #2</h3>
+      <Statistics title="Upload stats" stats={data} />
+      <h3>Task #3</h3>
+      <FriendList friends={friends} />;<h3>Task #4</h3>
+      <TransactionHistory items={transactions} />;
+    </>
+  );
+};
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,43 +1,37 @@
 import style from './statistics.module.css';
 import PropTypes from 'prop-types';
-// import bgcs from './colors.json'
 
-const Statistics = ({ title, stats })=> {
-  const elementsLi = stats.map(({ id, label, percentage }) => 
+const Statistics = ({ title, stats }) => {
+  const elementsLi = stats.map(({ id, label, percentage }) => (
     <li key={id} className={style.item}>
       <span className={style.label}>..{label}</span>
       <span className={style.percentage}>{percentage}%</span>
-    </li> 
-    )
-  // const bgcsLength = bgcs.length
-  // for (let i=0 ; i < elementsLi.length; i+= 1) {
-  //   i = i % bgcsLength
-  //   console.log(i)
-  // }
+    </li>
+  ));
 
   return (
     <section className={style.statistics}>
       {title && <h2 className={style.title}>{title}</h2>}
 
-      <ul className={style.list}>
-        {elementsLi}
-      </ul>
+      <ul className={style.list}>{elementsLi}</ul>
     </section>
-  )
-}
+  );
+};
 
 export default Statistics;
 
 Statistics.defaultProps = {
-  title: "",
-  stats: []
-}
+  title: '',
+  stats: [],
+};
 
 Statistics.propTypes = {
-  title: PropTypes.string, 
-  stats: PropTypes.arrayOf(PropTypes.shape({
-    id: PropTypes.string.isRequired , 
-    label: PropTypes.string.isRequired , 
-    percentage: PropTypes.number.isRequired
-  }))
-}
+  title: PropTypes.string,
+  stats: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+      percentage: PropTypes.number.isRequired,
+    })
+  ),
+};
